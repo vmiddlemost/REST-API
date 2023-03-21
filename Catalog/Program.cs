7f78fd9a8435625dd5eea51f0eac8b7d678e5ad2
vmiddlemost <68695177+vmiddlemost@users.noreply.github.com>
2023-03-21 17:21:27 +0000

    Docker

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -47,7 +47,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 
 app.UseAuthorization();
 
